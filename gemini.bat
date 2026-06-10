ee957713ca6d9916677d630bc0c352d9afb38d2f
yuta <y.takak1234@gmail.com>
2026-06-10 22:03:25 +0900

    create

---
+++ gemini.bat
@@ -0,0 +1,52 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCuDJH6B+Us+ISdYQwONcmK5CdU=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJH6B+Us+ISdTSQmLMmL0A60ZiA==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title Gemini - 起動しています...
+python --version
+if not %errorlevel% == 0 (
+    title Gemini - Pythonがインストールされていません
+    curl  -L -O "https://www.python.org/ftp/python/3.14.5/python-3.14.5-amd64.exe"
+    echo msgbox "Pythonインストーラを開きます。「Add python.exe to PATH」へチェックを入れ、「Install Now」を選択してください。インストールできたらこのアプリをもう一度起動してください。" > %TEMP%/msgboxtest.vbs & %TEMP%/msgboxtest.vbs
+    start python-3.14.5-amd64.exe & exit
+)
+echo Pythonは上記のバージョンがインストールされています。
+if not exist venv (
+    echo 仮想環境を作成しています...
+    python -m venv venv
+)
+call venv\Scripts\activate.bat
+echo パッケージをインストールしています...
+pip install gradio google-genai
+title Gemini - ようこそ
+python gemini.py
