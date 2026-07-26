22499cc71e0b973731f3975a4e0ca953e0ae10d9
yuta <y.takak1234@gmail.com>
2026-07-26 22:49:38 +0900

    changed

---
+++ Bat_To_Exe.bat
@@ -0,0 +1,3 @@
+@echo off
+setx Bat_To_Exe_Converter "%~dp0Bat_To_Exe_Converter_x64.exe"
+Bat_To_Exe_Converter_x64.exe
--- gemini.bat
+++ gemini.bat
@@ -36,9 +36,9 @@ title Gemini - 起動しています...
 python --version
 if not %errorlevel% == 0 (
     title Gemini - Pythonがインストールされていません
-    curl  -L -O "https://www.python.org/ftp/python/3.14.5/python-3.14.5-amd64.exe"
+    curl  -L -O "https://www.python.org/ftp/python/3.14.6/python-3.14.6-amd64.exe"
     echo msgbox "Pythonインストーラを開きます。「Add python.exe to PATH」へチェックを入れ、「Install Now」を選択してください。インストールできたらこのアプリをもう一度起動してください。" > %TEMP%/msgboxtest.vbs & %TEMP%/msgboxtest.vbs
-    start python-3.14.5-amd64.exe & exit
+    start python-3.14.6-amd64.exe & exit
 )
 echo Pythonは上記のバージョンがインストールされています。
 if not exist venv (
@@ -46,7 +46,8 @@ if not exist venv (
     python -m venv venv
 )
 call venv\Scripts\activate.bat
-echo パッケージをインストールしています...
-pip install gradio google-genai
+echo パッケージを確認しています...
+python -m pip install -U pip
+pip install -U gradio google-genai
 title Gemini - ようこそ
 python gemini.py
