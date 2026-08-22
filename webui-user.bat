d611685a0c08a8a2cda57f7b1cef5ca5f83f94f2
yuta <y.takak1234@gmail.com>
2026-08-23 06:20:05 +0900

    changed

--- webui-user.bat
+++ webui-user.bat
@@ -1,6 +1,40 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFAJATQ+DAHiuB7cQ7aX/5uyBt14UW+xyb4rZz72yLO8U5QvtdplN
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFAJATQ+DAHiuB7cQ7aX/5uyBt14UW+xyb4rZz72yN+UR/0ypYIUoxDRfgM5s
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 title Stable Diffusion WebUI
 set PYTHON="C:\Users\%username%\AppData\Local\Programs\Python\Python310\python.exe"
+set STABLE_DIFFUSION_REPO=https://github.com/w-e-w/stablediffusion.git
 
 nvidia-smi
 if %ERRORLEVEL% NEQ 0 (
@@ -12,7 +46,7 @@ if %ERRORLEVEL% NEQ 0 (
 git config --global --add safe.directory %~dp0\repositories\*
 if %ERRORLEVEL% NEQ 0 (
     winget install --id Git.Git -e --source winget
-    echo Git‚ðƒCƒ“ƒXƒg[ƒ‹‚µ‚Ü‚µ‚½B‚à‚¤ˆê“x‚±‚ÌƒAƒvƒŠ‚ð‹N“®‚·‚é•K—v‚ª‚ ‚é‚©‚à‚µ‚ê‚Ü‚¹‚ñB
+    echo Gitï¿½ï¿½ï¿½Cï¿½ï¿½ï¿½Xï¿½gï¿½[ï¿½ï¿½ï¿½ï¿½ï¿½Ü‚ï¿½ï¿½ï¿½ï¿½Bï¿½ï¿½ï¿½ï¿½ï¿½ï¿½xï¿½ï¿½ï¿½ÌƒAï¿½vï¿½ï¿½ï¿½ï¿½ï¿½Nï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Kï¿½vï¿½ï¿½ï¿½ï¿½ï¿½é‚©ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ü‚ï¿½ï¿½ï¿½B
 )
 
 call webui.bat
