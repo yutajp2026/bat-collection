cfd4c4b3e2d4bf4706f79fabb0d3460243e062c9
yuta <y.takak1234@gmail.com>
2026-08-28 09:53:52 +0900

    changed

--- webui-user.bat
+++ webui-user.bat
@@ -37,9 +37,9 @@ set PYTHON="C:\Users\%username%\AppData\Local\Programs\Python\Python310\python.e
 
 nvidia-smi
 if %ERRORLEVEL% NEQ 0 (
-    set COMMANDLINE_ARGS=--use-cpu all --precision full --no-half --skip-torch-cuda-test
+    set COMMANDLINE_ARGS=--use-cpu all --precision full --no-half --skip-torch-cuda-test --log-startup --loglevel WARNING
 ) else (
-    set COMMANDLINE_ARGS=--xformers
+    set COMMANDLINE_ARGS=--xformers --log-startup --loglevel WARNING
 )
 
 git config --global --add safe.directory %~dp0\repositories\*
