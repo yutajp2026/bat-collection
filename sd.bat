618e4684cd6e0b55335643a0827835a4e71fcb24
yuta <y.takak1234@gmail.com>
2026-09-06 09:04:07 +0900

    changed

--- sd.bat
+++ sd.bat
@@ -2,5 +2,5 @@
 if not exist venv (python -m venv venv)
 call venv\Scripts\activate.bat
 pip install diffusers transformers translate
-pip install torch torchvision --index-url https://download.pytorch.org/whl/cu132
+pip install torch torchvision
 python main.py
